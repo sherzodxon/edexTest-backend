43806d9601155c9a71be255dcda5582c4ee111d1
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2026-01-27 19:52:56 +0500

    cors is edited

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://test.edexschool.uz/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
